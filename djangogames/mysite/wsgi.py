b9a87e9e3f91561b39b4541617fc71326cb3979d
MinCui310 <37454949+MinCui310@users.noreply.github.com>
2019-02-25 12:32:56 -0600

    Update wsgi.py

--- djangogames/mysite/wsgi.py
+++ djangogames/mysite/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+#application = get_wsgi_application()
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/cuimin/project-1'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
